b5a13714636842655b1dde9add400ac2343e8858
Henrik Bohlin <olleorm89@gmail.com>
2015-01-29 16:14:49 +0100

    now you can customize between witch numbers you want to play the game.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,15 +9,19 @@ import javafx.scene.input.KeyEvent;
 import java.util.Random;
 
 public class Controller {
+    @FXML private Label guessLabel;
     @FXML private Label numberGuessLabel;
     @FXML private TextField txtAddItem;
+    @FXML private TextField fromAddItem;
+    @FXML private TextField tooAddItem;
     @FXML private Label systemOut;
     static Random rand = new Random();
-    static final int RANDOM_NUMBER_FROM = 1;
-    static final int RANDOM_NUMBER_TO = 10;
-    int theNumber = randomInt(RANDOM_NUMBER_FROM, RANDOM_NUMBER_TO);
-    int guess;
-
+    int randomNumberFrom = 1;
+    int randomNumberTo = 10;
+    String textIn;
+    int theNumber = randomInt(randomNumberFrom, randomNumberTo);
+    int numberIn;
+    boolean isInt;
     int numberGuess = 0;
 
     public static int randomInt(int from, int to) {
@@ -26,43 +30,61 @@ public class Controller {
 
     public int countGuess(int theNumber) {
         numberGuess++;
-        if (guess < theNumber) {
+        if (numberIn < theNumber) {
             systemOut.setText("Too low");
             numberGuessLabel.setText("Guess counter:" + numberGuess);
-        } else if (guess > theNumber) {
+        } else if (numberIn > theNumber) {
             systemOut.setText("Too high");
             numberGuessLabel.setText("Guess counter:" + numberGuess);
         } else {
             systemOut.setText("Congratz you were right!");
             numberGuessLabel.setText("Guess counter:" + numberGuess);
             return numberGuess;
-
         }
         return numberGuess;
     }
 
-    public void guessing(){
-        String textIn = (txtAddItem.getText());
+    public int textInt(TextField x) {
+        textIn = (x.getText());
         if(!textIn.matches("^\\d+$")) {
             systemOut.setText("Pleas enter a number");
-        }else {
-            guess = Integer.parseInt(textIn);
-            if (guess < RANDOM_NUMBER_FROM || guess > RANDOM_NUMBER_TO){
-                systemOut.setText("Pleas enter number: " + RANDOM_NUMBER_FROM + "-" + RANDOM_NUMBER_TO);
-            }else {
+            isInt = false;
+            return 0;
+        }else{
+        isInt = true;
+        return Integer.parseInt(textIn);
+        }
+    }
+
+    public void guessing(){
+        numberIn = textInt(txtAddItem);
+        if (isInt == true) {
+            if (numberIn < randomNumberFrom || numberIn > randomNumberTo) {
+                systemOut.setText("Pleas enter number: " + randomNumberFrom + "-" + randomNumberTo);
+            } else {
                 numberGuess = countGuess(theNumber);
             }
         }
     }
 
     @FXML private void getNewNumber(ActionEvent action){
-        numberGuess = 0;
-        numberGuessLabel.setText("Guess counter:" + numberGuess);
-        theNumber = randomInt(RANDOM_NUMBER_FROM, RANDOM_NUMBER_TO);
+        int fromInt = textInt(fromAddItem);
+        if (isInt == true) {
+            int tooInt = textInt(tooAddItem);
+            if (isInt == true && fromInt < tooInt) {
+                numberGuess = 0;
+                numberGuessLabel.setText("Guess counter:" + numberGuess);
+                theNumber = randomInt(fromInt, tooInt);
+                guessLabel.setText("Guess a number:" + fromInt + "-" + tooInt);
+            } else{
+                systemOut.setText("The second number must be bigger then the first");
+            }
+        }
+
+
     }
 
-    @FXML
-    public void handleEnterPressed(KeyEvent event) {
+    @FXML public void handleEnterPressed(KeyEvent event) {
         if (event.getCode() == KeyCode.ENTER) {
             guessing();
         }

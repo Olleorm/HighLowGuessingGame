a9788b3a2ab4852b14dfc563ad6070fa9254c63f
Henrik Bohlin <olleorm89@gmail.com>
2015-01-29 21:27:20 +0100

    renamed file

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("highlow_view.fxml"));
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
